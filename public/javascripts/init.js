5d662afb8b7fb0533652a820f6947e573bc369d0
Alexander Atallah <aatallah@stanford.edu>
2014-02-05 19:49:59 -0800

    added jquery and bootstrap

---
+++ public/javascripts/init.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+  initPage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initPage() {
+  // add any functionality and listeners you want here
+}
