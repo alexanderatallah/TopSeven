897f94c1d6a814c2ca941bcf4702e5aa2ebca88e
Parth <ppatel3@stanford.edu>
2014-02-13 00:45:03 -0800

    swiping

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -4,13 +4,42 @@
 $(document).ready(function() {
   initPage();
   articleClick();
-})
+
+});
 
 /*
  * Function that is called when the document is ready.
  */
 function initPage() {
   // add any functionality and listeners you want here
+
+  /*$('#swipe').swiperight(function(e){
+  	alert('You swiped right');
+  });*/
+  $('#swipe').swipe({
+  	swipe:function(event,direction,distance,duration,fingerCount) {
+  		event.preventDefault();
+  		console.log(direction);
+  		$(this).hide();
+  		if(direction == 'right'){
+  			//move JSON object article to the "saved articles" list
+
+  		}
+  		if(direction == 'left'){
+  			//move JSON object article to the "deleted articles" list
+
+  		}
+  	},
+  	tap:function(event,target){
+  		//console.log(event);
+  		console.log($(target).closest('a').href);
+  	}
+  });
+
+  $('#swipe').click(function(e) {
+  	e.preventDefault();
+  })
+
 }
 
 function articleClick() {
