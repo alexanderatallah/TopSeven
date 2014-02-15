decfb750ae6651c527e4a674c7ff3117db3f47c2
Alexander Atallah <aatallah@stanford.edu>
2014-02-14 18:48:01 -0800

    many many bug fixes for saved and delete, code refactor

---
+++ public/javascripts/Seven.js
@@ -0,0 +1,168 @@
+'use strict';
+
+window.Seven = {
+  SEVEN: 7,
+
+  /**
+   * Saves articles
+   * 
+   * @public
+   */
+  saveArticles: function(articles) {
+    localStorage['articles'] = JSON.stringify(articles);
+  },
+
+  saveSavedArticles: function(articles) {
+    localStorage['saved-articles'] = JSON.stringify(articles);
+  },
+
+  saveDeletedArticles: function(articles) {
+    localStorage['deleted-articles'] = JSON.stringify(articles);
+  },
+
+  /**
+   * Loads articles
+   * 
+   * @public
+   */
+  loadArticles: function() {
+    return this.load_('articles');
+  },
+
+  loadSavedArticles: function() {
+    return this.load_('saved-articles');
+  },
+
+  loadDeletedArticles: function() {
+    return this.load_('deleted-articles')
+  },
+
+  load_: function(namespace) {
+    localStorage[namespace] = localStorage[namespace] || JSON.stringify([]);
+    return JSON.parse(localStorage[namespace]);
+  },
+
+  /**
+   * Gets an article by id
+   *
+   * @optional _cachedArticles, a cached JSON version
+   *
+   * @public
+   */
+  getArticle: function(id, _cachedArticles) {
+    var articles = _cachedArticles || this.loadArticles();
+    for (var i = 0; i < articles.length; i++) {
+      var article = articles[i];
+      if (article.id == id) return article;
+    }
+    return null;
+  },
+
+  /**
+   * Marks an article as deleted, removing it from the list
+   *
+   * @optional articles, a cached JSON version
+   *
+   * @public
+   */
+  deleteArticle: function(article) {
+    var deleted = this.loadDeletedArticles();
+    deleted.unshift(article);
+    this.saveDeletedArticles(deleted);
+
+    this.markAsSeen_(article);
+  },
+
+  saveArticle: function(article) {
+    var saved = this.loadSavedArticles();
+    saved.unshift(article);
+    this.saveSavedArticles(saved);
+
+    this.markAsSeen_(article);
+  },
+
+  isSeen: function(article) {
+    var seen = this.loadSeenArticles_();
+    return seen[article.id] == true;
+  },
+
+  // @private
+  markAsSeen_: function(article) {
+    var seen = this.loadSeenArticles_();
+    seen[article.id] = true;
+    localStorage['seen'] = JSON.stringify(seen);
+  },
+
+  // @private
+  loadSeenArticles_: function() {
+    localStorage['seen'] = localStorage['seen'] || JSON.stringify({});
+    return JSON.parse(localStorage['seen']);
+  },
+
+
+
+
+
+
+
+
+
+
+  //////////////////////////////////////////////
+  ////////////// New York Times ////////////////
+  //////////////////////////////////////////////
+
+  API_KEY_: "524a862fb70db9b6a0202562125bd2eb:3:56094140",
+
+  /**
+   * NYT URL that will give us lots and lots of whatever we're looking for.
+   * 
+   * @private
+   */
+  mostViewedURL_: function() {
+    return "http://api.nytimes.com/svc/mostpopular/v2/" + 
+    "mostviewed/all-sections/7.jsonp?api-key=" + 
+    this.API_KEY_;
+  },
+
+
+  /**
+   * Sends an XHR GET request
+   * 
+   * @public
+   */
+  fetchArticles: function(callback) {
+    var nyt = this;
+    var fetchArticlesCb = function(res) {
+      nyt.parseArticles_(res, callback);
+    }
+    $.get(this.mostViewedURL_(), fetchArticlesCb, 'jsonp');
+  },
+
+  /**
+   * Parses NYT articles, adding rank, etc.
+   *
+   * @private
+   */
+  parseArticles_: function(data, callback) {
+    console.log(data);
+    var articles = data.results;
+
+    for(var i = 0; i < articles.length; i++) {
+      var article = articles[i];
+      article.rank = this.rankArticle(article, i);
+    }
+
+    this.saveArticles(articles);
+    callback(articles);
+  },
+
+  /**
+   * Ranks an article
+   * @optional _i index of article in list
+   * @public
+   */
+  rankArticle: function(article, _i) {
+    return article.views || ((_i || 0) + 1);
+  }
+}
--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,52 +1,13 @@
 'use strict';
 
-var SEVEN = 7;
-
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-  initArticles();
   articleClick();
-  swiping();
   $("#refresher").click(refreshArticles);
 });
 
-function initArticles() {
-  var articles = NYT.loadArticles();
-  if (!articles || articles.length == 0) refreshArticles();
-  else replaceArticles(articles);
-}
-
-function swiping() {
-  $('.swipe').swipe({
-    swipe:function(event,direction,distance,duration,fingerCount) {
-      event.preventDefault();
-      console.log(direction);
-      var id = $(this).closest('a')[0].href.substring(33);
-      console.log(id);
-      $(this).hide();
-      var article = NYT.getArticle(id);
-      
-      if(direction == 'right'){
-        //move JSON object article to the "saved articles" list
-        NYT.addSavedArticle(article);  
-      }
-      if(direction == 'left'){
-        //move JSON object article to the "deleted articles" list
-        NYT.deleteArticle(article);
-      }
-    },
-    tap:function(event,target){
-      window.location = $(target).closest('a')[0].href;
-    }
-  });
-
-  $('.swipe').click(function(e) {
-    e.preventDefault();
-  });
-}
-
 function articleClick() {
-  $(".article-list .list-group-item").click(function() {
+  $(".article-list").on("click", ".list-group-item", function() {
     $(this).closest(".article-list")
       .find(".list-group-item.active")
       .removeClass("active");
@@ -57,13 +18,38 @@ function articleClick() {
 function refreshArticles() {
   var spinner = $("#refresher").find(".spinner-inline");
   spinner.addClass("spin");
-  NYT.fetchArticles(function(results) {
+  Seven.fetchArticles(function(results) {
     spinner.removeClass("spin");
     replaceArticles(results);
   });
 }
 
-function replaceArticles(articles) {
+/*
+ * THESE FUNCTIONS BELOW ARE CALLED FROM SCRIPTS BELOW
+ * THE HTML IN THE HANDLEBARS FILES, FOR RESPECTIVE PAGES
+ *
+ */
+function initArticles() {
+  var articles = Seven.loadArticles();
+  if (!articles || articles.length == 0) refreshArticles();
+  else replaceArticles(articles);
+}
+
+function initSavedArticles() {
+  var saved = Seven.loadSavedArticles();
+  if (!saved || saved.length == 0) return;
+  else replaceArticles(saved, false);
+}
+
+function initDeletedArticles() {
+  var deleted = Seven.loadDeletedArticles();
+  if (!deleted || deleted.length == 0) return;
+  else replaceArticles(deleted, false);
+}
+
+function replaceArticles(articles, _excludeSeen) {
+  if (_excludeSeen === undefined) _excludeSeen = true;
+
   var template = function(article) {
     var thumbnail = "";
     if (article.media && article.media[0].type=="image") {
@@ -79,16 +65,45 @@ function replaceArticles(articles) {
   }
 
   var articleList = "";
-  for (var i = 0; i < SEVEN; i++) {
+  var limit = _excludeSeen ? Seven.SEVEN : articles.length;
+  for (var i = 0; i < limit; i++) {
     var article = articles[i];
+    if (_excludeSeen && Seven.isSeen(article)) continue;
     articleList += template(article);
   }
 
-  $("#articleList").find('.list-group')
+  $(".article-list").find('.list-group')
     .html(articleList);
+
+  enableSwiping();
 }
 
- //EASILY GENERALIZABLE TO POPULATE SAVED + DELETED LISTS
-  
+function enableSwiping() {
+  $('.swipe').swipe({
+    swipe:function(event,direction,distance,duration,fingerCount) {
+      event.preventDefault();
+      console.log(direction);
+      var id = $(this).closest('a')[0].href.substring(33);
+      console.log(id);
+      $(this).hide();
+      var article = Seven.getArticle(id);
+      
+      if(direction == 'right'){
+        //move JSON object article to the "saved articles" list
+        Seven.saveArticle(article);  
+      }
+      if(direction == 'left'){
+        //move JSON object article to the "deleted articles" list
+        Seven.deleteArticle(article);
+      }
+    },
+    tap:function(event,target){
+      window.location = $(target).closest('a')[0].href;
+    }
+  });
 
+  $('.swipe').click(function(e) {
+    e.preventDefault();
+  });
+}
 
