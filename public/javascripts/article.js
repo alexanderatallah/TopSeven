cd1766ee09401472e26d663ee8e168e942293be8
Alexander Atallah <aatallah@stanford.edu>
2014-02-14 18:50:27 -0800

    fixed article view

--- public/javascripts/article.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-$(document).ready(function() {
-  var article = NYT.getArticle(getParam("id"));
-  $("#articleTitle").text(article.title);
-  $("#articleFrame").attr('src', article.url);
-});
